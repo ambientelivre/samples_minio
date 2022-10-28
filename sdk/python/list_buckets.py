3d29b1a6200337edf74cf71f94fac268e5d0dfaa
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2022-10-27 21:53:48 -0300

    Update list_buckets.py

--- sdk/python/list_buckets.py
+++ sdk/python/list_buckets.py
@@ -1,3 +1,6 @@
 from minio import Minio
-from connection import connection
-print client.list_buckets
+from connection import client
+
+buckets = client.list_buckets()
+for bucket in buckets:
+    print(bucket.name, bucket.creation_date)
